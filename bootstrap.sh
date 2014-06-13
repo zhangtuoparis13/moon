70296214363d136e558d82fbadf5b208b3eef9a9
dthom <thomas.duval@orange.com>
2014-06-13 16:49:30 +0200

    Add Vagrant files to test Moon framework

---
+++ bootstrap.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+MySQLPASSWD="P4ssw0rd"
+
+apt-get update
+apt-get install -y language-pack-fr
+export LANGUAGE=fr_FR
+export LANG=fr_FR.UTF-8
+export LC_ALL=fr_FR.UTF-8
+locale-gen fr_FR.UTF-8
+dpkg-reconfigure locales
+
+echo "mysql-server-5.5 mysql-server/root_password password ${MySQLPASSWD}
+mysql-server-5.5 mysql-server/root_password seen true
+mysql-server-5.5 mysql-server/root_password_again password ${MySQLPASSWD}
+mysql-server-5.5 mysql-server/root_password_again seen true
+" | debconf-set-selections
+apt-get install -y --force-yes mysql-server
+
+apt-get install -y --force-yes python-minimal python-mysqldb python-django python-dev python-pip graphviz python-sqlalchemy python-pygraphviz
+
+pip install django_openstack_auth
+pip install python-keystoneclient
+pip install python-novaclient
+
+mysql -uroot -p$MySQLPASSWD <<EOF
+create database user_db;
+create user moonuser identified by '$MySQLPASSWD';
+grant all privileges on user_db.* to 'moonuser'@'localhost' identified by "$MySQLPASSWD" with grant option;
+create database moon;
+grant all privileges on moon.* to 'moonuser'@'localhost' identified by "$MySQLPASSWD" with grant option;
+EOF
+
+
+ln -s /vagrant/ /usr/local/lib/python2.7/dist-packages/moon
+
+ln -s /vagrant/samples/moon /etc/moon
+
+mkdir /var/log/moon/
+chown vagrant /var/log/moon/
