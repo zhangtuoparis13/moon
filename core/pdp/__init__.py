383081cacd10974b81849cb519803813bcef2141
Ruan HE <rebirthmonkey@gmail.com>
2014-09-12 11:50:53 +0200

    Enable the ling for PAP.

--- core/pdp/__init__.py
+++ core/pdp/__init__.py
@@ -1,4 +1,4 @@
 # from moon.core.pdp.authz import enforce, save_auth, toggle_auth_flag, toggle_init_flag
-# from moon.core.pdp.inter_extension import get_inter_extensions
-# from moon.core.pdp.core import get_intra_extensions
+from moon.core.pdp.inter_extension import get_inter_extensions
+from moon.core.pdp.core import get_intra_extensions
 from moon.core.pdp.extension import Extension
