6106f6e3f187d33b77c7254f13f90121aed02128
dthom <thomas.duval@orange.com>
2014-09-17 12:25:34 +0200

    Fix a bug when calling add_rule

--- core/pap/core.py
+++ core/pap/core.py
@@ -177,7 +177,7 @@ class PAP:
     def add_rule(self, extension_uuid, user_uuid, sub_cat_value, obj_cat_value):
         if extension_uuid in self.intra_extensions.keys():
             if self.intra_extensions[extension_uuid].admin(user_uuid, "rules", "write") == "OK":
-                self.intra_extensions[extension_uuid].intra_extension_authz.add_rules(sub_cat_value, obj_cat_value)
+                self.intra_extensions[extension_uuid].intra_extension_authz.add_rule(sub_cat_value, obj_cat_value)
 
     # def del_rule(self, extension_uuid, user_uuid, rule_id):
     #     if extension_uuid in self.intra_extensions.keys():
