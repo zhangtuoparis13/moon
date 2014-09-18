46237d6a6c6461c6b72b77429958be0715c0b904
Ruan HE <rebirthmonkey@gmail.com>
2014-09-18 17:30:01 +0200

    core/pdp/core/authz() create

--- core/pdp/core.py
+++ core/pdp/core.py
@@ -18,6 +18,12 @@ class IntraExtensions:
         self.__installed_intra_extensions = dict()
         self.__syncer = IntraExtensionsSyncer()
 
+    def authz(self, sub, obj, act):
+        for _intra_extension in self.__installed_intra_extensions.values():
+            if _intra_extension.authz(sub, obj, act) == 'OK':
+                return 'OK'
+        return 'KO'
+
     def install_intra_extension_from_json(self, extension_setting_dir):
         extension_setting_abs_dir = extension_setting_dir
         if not os.path.isdir(extension_setting_dir):
@@ -73,7 +79,8 @@ class InterExtensions:  # TODO: to test
     def authz(self, requesting_intra_extension_uuid, requested_intra_extension_uuid, sub, obj, act):
         for _installed_inter_extension in self.__installed_inter_extensions.values():
             if _installed_inter_extension.check_requesters(requesting_intra_extension_uuid, requested_intra_extension_uuid):
-                return _installed_inter_extension.authhz(sub, obj, act)
+                if _installed_inter_extension.authhz(sub, obj, act) == 'OK':
+                    return 'OK'
         return "KO"
 
     def create_collaboration(self, requesting_intra_extension_uuid, requested_intra_extension_uuid,
@@ -109,3 +116,10 @@ def get_intra_extensions():
 
 def get_inter_extensions():
     return inter_extensions
+
+
+def authz(requesting_intra_extension_uuid, requested_intra_extension_uuid, sub, obj, act):
+    if requesting_intra_extension_uuid == requested_intra_extension_uuid:
+        return intra_extensions.authz(sub, obj, act)
+    else:
+        inter_extensions.authz(requesting_intra_extension_uuid, requested_intra_extension_uuid, sub, obj, act)
