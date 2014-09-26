f643c941a82607089cf4cd65f2964b3c5a499248
dthom <thomas.duval@orange.com>
2014-09-26 10:36:22 +0200

    Modify the HTTP responses to a dictionary

--- gui/views_pip.py
+++ gui/views_pip.py
@@ -13,46 +13,50 @@ logger = logging.getLogger("moon.django")
 @save_auth
 def tenants(request, tenant_uuid=None):
     pip = get_pip()
-    return HttpResponse(json.dumps(list(pip.get_tenants(uuid=tenant_uuid))))
+    return HttpResponse(json.dumps({"tenants": list(pip.get_tenants(uuid=tenant_uuid))}))
 
 
 @login_required(login_url='/auth/login/')
 @save_auth
 def subjects(request, tenant_uuid=None):
     pip = get_pip()
-    return HttpResponse(json.dumps(list(pip.get_subjects(tenant=tenant_uuid))))
+    return HttpResponse(json.dumps({"subjects": list(pip.get_subjects(tenant=tenant_uuid))}))
 
 
 @login_required(login_url='/auth/login/')
 @save_auth
 def objects(request, tenant_uuid=None):
     pip = get_pip()
-    return HttpResponse(json.dumps(list(pip.get_objects(tenant=tenant_uuid))))
+    return HttpResponse(json.dumps({"objects": list(pip.get_objects(tenant=tenant_uuid))}))
 
 
 @login_required(login_url='/auth/login/')
 @save_auth
 def roles(request, tenant_uuid=None, user_uuid=None):
     pip = get_pip()
-    return HttpResponse(json.dumps(list(pip.get_roles(tenant_uuid=tenant_uuid, uuid=user_uuid))))
+    return HttpResponse(json.dumps({"roles": list(pip.get_roles(tenant_uuid=tenant_uuid, uuid=user_uuid))}))
 
 
 @login_required(login_url='/auth/login/')
 @save_auth
 def groups(request, tenant_uuid=None, user_uuid=None):
     pip = get_pip()
-    return HttpResponse(json.dumps(list(pip.get_groups(tenant_uuid=tenant_uuid, uuid=user_uuid))))
+    return HttpResponse(json.dumps({"groups": list(pip.get_groups(tenant_uuid=tenant_uuid, uuid=user_uuid))}))
 
 
 @login_required(login_url='/auth/login/')
 @save_auth
 def role_assignments(request, tenant_uuid=None, user_uuid=None):
     pip = get_pip()
-    return HttpResponse(json.dumps(list(pip.get_users_roles_assignment(tenant_uuid=tenant_uuid, users=(user_uuid,)))))
+    return HttpResponse(json.dumps(
+        {"role_assignments": list(pip.get_users_roles_assignment(tenant_uuid=tenant_uuid, users=(user_uuid,)))}
+    ))
 
 
 @login_required(login_url='/auth/login/')
 @save_auth
 def group_assignments(request, tenant_uuid=None, user_uuid=None):
     pip = get_pip()
-    return HttpResponse(json.dumps(list(pip.get_users_groups_assignment(tenant_uuid=tenant_uuid, users=(user_uuid,)))))
+    return HttpResponse(json.dumps(
+        {"group_assignments": list(pip.get_users_groups_assignment(tenant_uuid=tenant_uuid, users=(user_uuid,)))}
+    ))
