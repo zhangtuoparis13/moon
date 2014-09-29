fff341e4b9350f95bd03622dfe262db47c6a062c
Ruan HE <rebirthmonkey@gmail.com>
2014-09-29 18:38:14 +0200

    tests/unit_test/unit_test.py create

--- tests/unit_test/unit_test.py
+++ tests/unit_test/unit_test.py
@@ -1,5 +1,5 @@
-#execfile("core_pdp_extension.py")
-#execfile("core_pdp_intra_extension.py")
-#execfile("core_pdp_inter_extension.py")
+execfile("core_pdp_extension.py")
+execfile("core_pdp_intra_extension.py")
+execfile("core_pdp_inter_extension.py")
 execfile("core_pap.py")
 
