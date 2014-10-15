c11c086edf1156dd264c9b0f7e38220d47d2862f
Ruan HE <rebirthmonkey@gmail.com>
2014-10-15 18:17:39 +0200

    moon/mrm/views v1 ok

--- mrm/views.py
+++ mrm/views.py
@@ -9,47 +9,49 @@ from moon import settings
 # TODO: this must be CSRF protected!!!
 
 
-def mrm_authz(request):
+def mrm_authz(post_request):
     """
-    post_data = [
-        ('requesting_tenant', requesting_tenant_uuid),
-        ('requested_tenant', requested_tenant_uuid),
-        ('object', object_uuid),
-        ('subject', subject_uuid),
-        ('action', action),
-        ('RAW_PATH_INFO', self._server_path),
-        ('key', key)
-    ]
+    post_request = {
+        'requesting_tenant': requesting_tenant_uuid,
+        'requested_tenant': requested_tenant_uuid,
+        'subject': subject_uuid,
+        'object': object_uuid,
+        'action', action,
+        'key': key
+    }
+    authz_response = {
+        'authz': "OK"/"KO",
+        'key': key
+    }
     """
 
-    response_data = {"authz": "KO"}
+    authz_response = {"authz": "KO"}
     try:
-        if request.method == 'POST':
-            crypt_key = hashlib.sha256()
-            if "key" in request.POST:
-                crypt_key.update(request.POST["key"])
+        if post_request.method == 'POST':
+            if "key" in post_request.POST:
+                crypt_key = hashlib.sha256()
+                crypt_key.update(post_request.POST["key"])
                 crypt_key.update(getattr(settings, "CNX_PASSWORD"))
-                response_data["key"] = crypt_key.hexdigest()
+                authz_response["key"] = crypt_key.hexdigest()
 
-            if request.POST["requesting_tenant"] and request.POST["requested_tenant"]:
+            if post_request.POST["requesting_tenant"] and post_request.POST["requested_tenant"]:
                 #  requesting_tenant and requested_tenant can not be None
-                if request.POST["requesting_tenant"] == request.POST["requested_tenant"]:
+                if post_request.POST["requesting_tenant"] == post_request.POST["requested_tenant"]:
                     _intra_manager = get_intra_extensions()
-                    _authz_result = _intra_manager.authz(
-                        subject=request.POST["subject"],
-                        object=request.POST["object"],
-                        action=request.POST["action"]
+                    authz_response["authz"] = _intra_manager.authz(
+                        subject=post_request.POST["subject"],
+                        object=post_request.POST["object"],
+                        action=post_request.POST["action"]
                     )
                 else:
                     _inter_manager = get_inter_extensions()
-                    _authz_result = _inter_manager.authz(
-                        requesting_tenant=request.POST["requesting_tenant"],
-                        requested_tenant=request.POST["requested_tenant"],
-                        subject=request.POST["subject"],
-                        object=request.POST["object"],
-                        action=request.POST["qction"]
+                    authz_response["authz"] = _inter_manager.authz(
+                        requesting_tenant=post_request.POST["requesting_tenant"],
+                        requested_tenant=post_request.POST["requested_tenant"],
+                        subject=post_request.POST["subject"],
+                        object=post_request.POST["object"],
+                        action=post_request.POST["action"]
                     )
-                response_data["authz"] = _authz_result
             else:
                 # raise Error Unknown Tenant
                 pass
@@ -58,4 +60,4 @@ def mrm_authz(request):
         import traceback
         print(sys.exc_info())
         print(traceback.print_exc())
-    return HttpResponse(json.dumps(response_data), content_type="application/json")
+    return HttpResponse(json.dumps(authz_response), content_type="application/json")
