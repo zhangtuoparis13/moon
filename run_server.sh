1de1e1c4d70a040f2c62f4a5db13685a99ce6cdc
Ruan HE <rebirthmonkey@gmail.com>
2014-10-17 14:47:46 +0200

    mrm v3

--- run_server.sh
+++ run_server.sh
@@ -2,4 +2,5 @@
 
 unset http_proxy
 
-python -m moon.moon_server --run runserver 0.0.0.0:8080
+# python -m moon.moon_server --run runserver 0.0.0.0:8080
+python -m moon.moon_server --run runserver 0.0.0.0:8888
