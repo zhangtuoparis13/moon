c7b04ec2c1d5da6531fe3378ed001326d5290731
Arnaud Marhin <arnaud.marhin@orange.com>
2014-10-21 10:13:57 +0200

    add intra-extension listing page, TODO: get associated tenant

--- gui/static/moon/app/intra-extension/intra-extension.js
+++ gui/static/moon/app/intra-extension/intra-extension.js
@@ -4,22 +4,133 @@
 
 angular.module('moonApp.intraExtension', ['ngTable', 'ngAnimate', 'mgcrea.ngStrap'])
 
-	 .config(function($stateProvider) {
+	.config(function($stateProvider) {
 		 
-		 $stateProvider
+		$stateProvider
 	        
-     	.state('moon.intraExtension', {
-             url: '/intraExtension',
-             controller: 'IntraExtensionController',
-             templateUrl: 'static/moon/app/intra-extension/intra-extension.tpl.html'
-         });
+		.state('moon.intraExtension', {
+			url: '/intraExtension',
+			controller: 'IntraExtensionController',
+			templateUrl: 'static/moon/app/intra-extension/intra-extension.tpl.html'
+		});
 		 
-	 })
+	})
 	 
-	 .controller('IntraExtensionController', ['$scope', function ($scope) {
+	.controller('IntraExtensionController', ['$q', '$scope', '$filter', '$modal', '$translate', 'ngTableParams', 'alertService', 'intraExtensionService', 
+	  	                                   		function ($q, $scope, $filter, $modal, $translate, ngTableParams, alertService, intraExtensionService) {
 
-		 // TODO
+		$scope.form = {};
+		
+		$scope.intraExtensionsLoading = true;
+		$scope.intraExtensions = [];
+		
+		// key => intraExtension | value: { loading: true/false, tenant: ... }
+		$scope.tenantsAssociatedToIntraExtensions = {};
+						
+		/*
+		 * list
+		 */
+		
+		var getData = function() {
+			 return $scope.intraExtensions;
+		};  	
+   	
+		$scope.intraExtensionsTable = new ngTableParams({
+	    
+			page: 1,            // show first page
+			count: 10,          // count per page
+			sorting: {
+				name: 'asc' // initial sorting
+			}
+   	
+		}, {
+	    	
+			total: function () { return getData().length; }, // length of data
+			getData: function($defer, params) {
+	        	
+				var orderedData = params.sorting() ?
+						
+					$filter('orderBy')(getData(), params.orderBy()) : getData();
+					$defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+	        	
+			},
+			$scope: { $data: {} }
+	        
+		});
+		
+		$scope.reloadTable = function() {
+			
+			$scope.intraExtensionsTable.total($scope.intraExtensions.length);
+			$scope.intraExtensionsTable.reload();
+			
+		};
+				   				
+		intraExtensionService.intraExtension.query({}).$promise.then(function(data) {
+			
+			var promises = [];
+			
+			_.each(data.intra_extensions, function(id) {
+				
+				promises.push($q(function(resolve) {
+												
+					intraExtensionService.intraExtension.get({ie_uuid: id}).$promise.then(function(data) {
+						resolve(data.intra_extensions);
+					});
+											
+				}));
+				
+			}); 
+											    
+		    $q.all(promises).then(function(intraExtensions) {
+		    	
+		    	$scope.intraExtensions = intraExtensions;
+		    			    	
+		    	$scope.intraExtensionsLoading = false;
+		    	$scope.reloadTable();
+		    	
+		    });
+			
+		});
+		
+		/*
+		 * add
+		 */
+		
+		
+		
+		/*
+		 * delete
+		 */
+		
+		
+		
+		/*
+		 * view
+		 */
+		
+		
 		 
-	 }])
+	}])
+	
+	.factory('intraExtensionService', function($resource) { 
+		
+		return {
+			
+			intraExtension: $resource('./json/intra-extensions/:ie_uuid', {}, {
+     	   		query: { method: 'GET', isArray: false },
+     	   		get: { method: 'GET', isArray: false }
+    	   	}),
+    	   	
+    	   	tenant: $resource('./json/intra-extensions/:ie_uuid/tenant', {}, {
+     	   		query: { method: 'GET', isArray: false }
+    	   	}),
+     	   	
+			policy: $resource('./json/intra-extensions/policies', {}, {
+     	   		query: { method: 'GET', isArray: false }
+    	   	})
+    	   	
+		};
+		
+	})
 	 
 ;
