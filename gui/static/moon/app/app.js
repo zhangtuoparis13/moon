2f2c63ddacc781753b78362225f033d8e366fb89
Arnaud Marhin <arnaud.marhin@orange.com>
2014-10-27 13:57:22 +0100

    refactor: main app module

--- gui/static/moon/app/app.js
+++ gui/static/moon/app/app.js
@@ -2,73 +2,90 @@
  * @author arnaud marhin<arnaud.marhin@orange.com>
  */
 
-'use strict';
+(function() {
 
-var moonApp = angular.module('moonApp', ['ngRoute',                                            
-                                         'ui.router',
-                                         'ui.bootstrap',
-                                         'ngTable',
-                                         'ngCookies',
-                                         'pascalprecht.translate',
-                                         'ngAnimate',
-                                         'mgcrea.ngStrap',
-                                         'ui.select',
-                                         'moonApp.common',
-                                         'moonApp.tenant',
-                                         'moonApp.intraExtension',
-                                         'moonApp.interExtension',
-                                         'moonApp.nova'])
+	'use strict';
 
-    .config(['$urlRouterProvider', '$translateProvider', '$stateProvider', 'uiSelectConfig',
-        function($urlRouterProvider, $translateProvider, $stateProvider, uiSelectConfig) {
-    	
-    		$urlRouterProvider.when('', '/tenant');
-    		
-            $urlRouterProvider.otherwise('/404');
+	angular
+	
+		.module('moon', ['ngRoute',                                            
+                         'ui.router',
+                         'ui.bootstrap',
+                         'ngTable',
+                         'ngCookies',
+                         'pascalprecht.translate',
+                         'ngAnimate',
+                         'mgcrea.ngStrap',
+                         'ui.select',
+                         'moonApp.common',
+                         'moonApp.tenant',
+                         'moonApp.intraExtension',
+                         'moonApp.interExtension',
+                         'moonApp.nova'])
 
-            $stateProvider.state('moon', {
-	            abstract: true,
-	            template: '<div ui-view></div>'
-	        })
-            
-            $stateProvider.state('moon.404', {
-            		url: '/404',
-                    templateUrl: 'static/moon/app/common/404.tpl.html'
-                });
+    .config(configurer)
+    .run(runner);
+	
+	/*
+	 * configurer
+	 */
+	
+	configurer.$inject = ['$urlRouterProvider', '$translateProvider', '$stateProvider', 'uiSelectConfig'];
+	
+	function configurer($urlRouterProvider, $translateProvider, $stateProvider, uiSelectConfig) {
+		
+		$urlRouterProvider.when('', '/tenant');
+		
+        $urlRouterProvider.otherwise('/404');
 
-            $translateProvider
-                .useStaticFilesLoader({
-                    prefix: 'static/moon/i18n/',
-                    suffix: '.json'
-            })
-            .preferredLanguage('en')
-            .useCookieStorage();
-            
-            uiSelectConfig.theme = 'selectize';
-
-        }
-    
-    ])
-
-    .run(['$rootScope', '$state', '$modal',
-        function($rootScope, $state, $modal) {
-    	
-        	$rootScope.transitionModal = $modal({scope: $rootScope, template: 'static/moon/app/common/waiting.tpl.html', backdrop: 'static', show: false});
-    	
-            $rootScope.$on('$stateChangeStart', function (toState, toParams, fromState, fromParams) {
-                $rootScope.transitionModal.$promise.then($rootScope.transitionModal.show);
+        $stateProvider.state('moon', {
+            abstract: true,
+            template: '<div ui-view></div>'
+        })
+        
+        $stateProvider.state('moon.404', {
+        		url: '/404',
+                templateUrl: 'static/moon/app/common/404.tpl.html'
             });
 
-            $rootScope.$on('$stateChangeSuccess', function(){
-        		$rootScope.transitionModal.hide();
-            });
+        $translateProvider
+            .useStaticFilesLoader({
+                prefix: 'static/moon/i18n/',
+                suffix: '.json'
+        })
+        .preferredLanguage('en')
+        .useCookieStorage();
+        
+        uiSelectConfig.theme = 'selectize';
+		
+	};
+	
+	/*
+	 * runner
+	 */
+	
+	runner.$inject = ['$rootScope', '$state', '$modal'];
+	
+	function runner($rootScope, $state, $modal) {
+		
+		$rootScope.transitionModal = $modal({ scope: $rootScope, template: 'static/moon/app/common/waiting.tpl.html', backdrop: 'static', show: false });
+    	
+        $rootScope.$on('$stateChangeStart', stateChangeStart);
+        $rootScope.$on('$stateChangeSuccess', stateChangeSuccess);
+        $rootScope.$on('$stateChangeError', stateChangeError);
+        
+        function stateChangeStart(toState, toParams, fromState, fromParams) {
+            $rootScope.transitionModal.$promise.then($rootScope.transitionModal.show);
+        };
+        
+        function stateChangeSuccess() {
+    		$rootScope.transitionModal.hide();
+        };
+        
+        function stateChangeError() {
+    		$rootScope.transitionModal.hide();
+        };
+		
+	};
 
-            $rootScope.$on('$stateChangeError',  function(){
-        		$rootScope.transitionModal.hide();
-            });
-                        
-        }
-    
-    ]);
-
-;
+})();
