7784c912acf4861013d91e99abb5b7e1aab073c9
Arnaud Marhin <arnaud.marhin@orange.com>
2014-11-12 16:33:09 +0100

    add empty controller for rule deletion

---
+++ gui/static/moon/app/intra-extension/intra-extension.controller.rule.delete.js
@@ -0,0 +1,23 @@
+/**
+ * @author arnaud marhin<arnaud.marhin@orange.com>
+ */
+
+(function() {
+
+	'use strict';
+					
+	angular
+		.module('moon')
+			.controller('IntraExtensionDeleteRuleController', IntraExtensionDeleteRuleController);
+	
+	IntraExtensionDeleteRuleController.$inject = ['$scope', '$translate', 'alertService', 'intraExtensionService'];
+	
+	function IntraExtensionDeleteRuleController($scope, $translate, alertService, intraExtensionService) {
+		
+		var del = this;
+		
+				
+		
+	};
+	
+})();
