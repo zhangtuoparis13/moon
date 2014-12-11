e82e02c273623d15949b308bbe36f6b8db4e1eee
dthom <thomas.duval@orange.com>
2014-12-11 15:15:30 +0100

    Change setup script to create working tar.gz files

--- setup.py
+++ setup.py
@@ -3,23 +3,33 @@ import sys
 from setuptools import setup, find_packages
 from pkg_resources import resource_filename
 
+# In order to make the sdist, we have to delete os.link
+# from http://bugs.python.org/issue8876#msg208792
+del os.link
+
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing # noqa
+except ImportError:
+    pass
+
+
 confdir = resource_filename(__name__, "moon_server/samples/moon")
 
 setup(
-    name='Moon_server',
-    version='0.1.0',
-    packages=find_packages(),
-    author='DThom',
-    author_email="thomas.duval@orange.com",
-    url="https://github.com/rebirthmonkey/moon",
-    license='Apache License, Version 2.0',
-    long_description=open('README.md').read(),
-    # test_suite="tests",
-    package_dir={'moon': './'},
-    install_requires=['django_openstack_auth', 'python-keystoneclient', 'python-novaclient', 'pymongo'],
+    setup_requires=['pbr'],
+    pbr=True
 )
 
 if sys.argv[1] == "install" and os.name == "posix":
+    if not os.path.isfile("/usr/bin/mysql"):
+        print("MySQL must be installed, please run "
+              "'apt-get install mysql-server mysql-client mongodb-server python-pip python-dev libmysqlclient-dev' "
+              "before installing Moon...")
+        print("\033[31mMoon unconfigured...\033[m")
+        sys.exit(1)
     print("Copying {} to /etc".format(confdir))
     import shutil
     import subprocess
@@ -33,7 +43,7 @@ if sys.argv[1] == "install" and os.name == "posix":
     try:
         shutil.copytree(confdir, "/etc/moon")
     except OSError:
-        pass
+        print("Error while copying configuration files... Maybe they are already there...")
     # except IOError:
     #     shutil.copy(os.path.join(os.getcwd(), confdir, "api.json"), "/etc/moon/")
     # except OSError:
