d9ed2c93d0b247fd07480d804dfee593d9b3c467
mcompast <mcompast@isep.fr>
2015-07-09 10:33:46 +0200

    DevStack-over-Vagrant Added

---
+++ test/devstack-over-vagrant/README.rst
@@ -0,0 +1,105 @@
+Environnement de test Devstack
+==============================
+
+Pré-requis
+----------
+- Une connexion Internet fonctionnelle vers les dépôts APT Ubuntu & le cloud Hashicorp (https://atlas.hashicorp.com),
+- Oracle Virtualbox (https://www.virtualbox.org/),
+- Vagrant (https://www.vagrantup.com/),
+- un client SSH.
+
+Présentation:
+-------------
+Ce programme cherche a récupérer, installer et configurer automatiquement un environement Devstack au sein d'une machine virtuelle virtualbox.
+
+Plus précisément, le script réalise les étapes suivantes:
+
+- récupération d'une VM sous Ubuntu Trustry Tahr Server depuis les dépots d'hashicorp,
+
+- configuration du support du proxy dans la VM pour APT et pour le script d'installation,
+
+- mise à jour des index des dépots APT,
+
+- mise à jour des paquets du système,
+
+- installation du SCM GIT,
+
+- configuration du support du proxy par ce dernier,
+
+- récupération de DevStack,
+
+- patch de ce dernier pour utiliser le protocole HTTPS et non GIT,
+
+- configuration de DevStack,
+
+- lancement de DevStack.
+
+
+Ce programme est composé de deux fichiers:
+
+- le fichier ``VagrantFile`` indiquant:
+
+	+ la VM de base est à utiliser,
+
+	+ la configuration personnalisée à lui appliquer,
+
+	+ le script de provisionning à amorcer;
+
+- le script de ``devstack.provision.sh`` servant à installer les dépendances de DevStack dans la VM conçue, à l'installer, à le configurer et à l'amorcer:
+
+Configuration:
+--------------
+
+Avant d'amorcer la construction de la VM et son provisionning, il est nécéssaire de configurer quelques variables internes au script ``devstack.provision.sh``
+
+Proxy
+~~~~~
+
+================== =================
+Paramètres         Descriptions
+================== =================
+http_proxy         Définit l'URL du proxy pour les connexions HTTP
+https_proxy        Définit l'URL du proxy pour les connexions HTTPS
+ftp_proxy          Définit l'URL du proxy pour les connexions FTP 
+no_proxy           Definit les hôtes ne nécéssitant pas de connexion par proxy
+APT_CONF           Définit le chemin vers le fichier de configuration d'APT génrant les connexions par proxy.
+================== =================
+
+Source et destination de Devstack
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+================== =================
+Paramètres         Descriptions
+================== =================
+BRANCH             Branch de devstack à cloner (master|sable/kilo|stable/juno| etc.)
+URL_DEVSTACK       URL du dépot GIT DevStack à utiliser
+REP_REPO_DEVSTACK  Répertoire de destination où doit-être cloné DevStack
+================== =================
+
+Paramètres DevStack
+~~~~~~~~~~~~~~~~~~~
+
+================== =================
+Paramètres         Descriptions
+================== =================
+DEVSTACK_USER      Utilisateur allant exécuter l'exécutable Devstack. Il ne doit pas s'agir d'un super-utilisateur.
+FIXED_RANGE        Plage IP des réseaux virtuels des instances d'Openstack (en notation I.P.V.4/Masque)
+NETWORK_GATEWAY    Adresse de passerelle vers le réseaux exterieur dans le réseau virtuel
+LOGDAY             Durée, en jours, durant laquelles les journaux d'OpenStack sont gardés
+LOGDIR             Répertoire des fichiers journaux d'OpenStack
+LOGFILE            Chemin du fichier journal principal de d'OpenStack
+ADMIN_PASSWORD     Mots de passes des comptes admin et demo de l'installation d'OpenStack.
+DATABASE_PASSWORD  Mot de passe de l'utilisateur OpenStack en base de donnée
+RABBIT_PASSWORD    Mot de passe de l'utilisateur OpenStack du broker RabbitMQ
+SERVICE_PASSWORD   Mot de passe des services OpenStack, utilisé avec KeyStone 
+SERVICE_TOKEN      Token utilisé après l'authentification des services avec KeyStone
+CONF_FILE          Emplacement du fichier de configuration à configurer
+STACK_USER         Utilisateur sous lequel OpenStack sera exécuté
+================== =================
+
+Amorçage
+--------
+
+Après avoir configuré le script, la configuration et l'amorçage de l'environnement de test se fait grace à la commande suivante:
+
+``$ vagrant up``
---
+++ test/devstack-over-vagrant/devstack.provision.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+# Copyright 2015 Open Platform for NFV Project, Inc. and its contributors
+# This software is distributed under the terms and conditions of the 'Apache-2.0'
+# license which can be found in the file 'LICENSE' in this package distribution
+# or at 'http://www.apache.org/licenses/LICENSE-2.0'.
+
+# Ce  script cherche à provisionner une machine Ubuntu Trusty avec une installation de Devstack.
+# Les étapes réaliser par ce script sont les suivantes:
+# 	* Configuration du support du proxy par la VM par APT et le script d'installation
+#	* MAJ des index des dépots APT
+#	* MAJ des paquets du système
+#	* Installation du SCM GIT
+#	* Configuration du support du proxy par ce dernier
+#	* Récupération de DevStack
+#	* Patch de ce dernier pour utiliser le protocole HTTPS et non GIT
+#	* Configuration de DevStack
+#	* Lancement de DevStack
+# Ce présent script a été testé sur Ubuntu Trusty, avec une configuration dèrrière un serveur Proxy
+
+
+############################ Configuration du script #############################################
+## Proxy
+	## Info : Diverses variables sur le proxy
+http_proxy=
+https_proxy=
+ftp_proxy=
+no_proxy=localhost,127.0.0.0/8,::1
+	## Configuration du proxy dans APT
+APT_CONF=/etc/apt/apt.conf.d/20-proxy.conf
+	## Utilisateur allant executer Devstack
+DEVSTACK_USER=vagrant
+	## Branche de devstack à utiliser (master|stable/kilo|stable/juno etc.)
+BRANCH=master
+
+## Devstack SRC
+	# Url d'où GIT doit cloner le dépot devstack
+URL_DEVSTACK=https://github.com/openstack-dev/devstack.git
+	# Repertoire ou dépot devstack doit être cloné
+REP_REPO_DEVSTACK=~vagrant/devstack/
+
+## Devstack config
+FIXED_RANGE=10.254.1.0/24
+NETWORK_GATEWAY=10.254.1.1
+LOGDAY=1
+LOGDIR=\$DEST/logs
+LOGFILE=\$LOGDIR/stack.sh.log
+ADMIN_PASSWORD=mokJomRyxIjagDogCafVishec
+DATABASE_PASSWORD=huJautBoyzfucomFicuvTagRo
+RABBIT_PASSWORD=wohodofNavfogJiercisGujEe
+SERVICE_PASSWORD=amenhoucterkajOsawquavdis
+SERVICE_TOKEN=9f2d78d4-e679-b158-9ab7-4d2e01613d7b
+CONF_FILE=~vagrant/devstack/local.conf
+STACK_USER=vagrant
+################################# Fin de la configuration ########################################
+
+echo ====================== Début du provisonnement de devstack ========================
+echo 
+echo == Application de la configuration du proxy
+
+#Si on a déclarer un proxy qqpart, alors, on supprime le fichier de configuration du proxy
+if [ "$http_proxy" != "" ] ||  [ "$https_proxy" != "" ] || [ "$ftp_proxy" != "" ] ; then 
+	echo 	\* Suppression de la configuration pré-existante
+	rm -f APT_CONF
+fi
+
+# Application de la configuration du proxy à APT
+if [ "$http_proxy" != "" ]; then
+	echo 	\* Configuration APT - Proxy HTTP : $http_proxy 
+	echo Acquire::http::proxy \"$http_proxy\"\; >> $APT_CONF
+fi
+
+if [ "$https_proxy" != "" ]; then
+	echo 	\* Configuration APT - Proxy HTTPS : $https_proxy
+	echo Acquire::https::proxy \"$https_proxy\"\; >> $APT_CONF
+fi
+
+if [ "$ftp_proxy" != "" ]; then
+	echo 	\* Configuration APT - Proxy FTP : $ftp_proxy
+	echo Acquire::ftp::proxy \"$ftp_proxy\"\; >> $APT_CONF
+fi
+
+echo == Mise à jour de APT
+echo 	\*  Mise à jour des dépots
+apt-get update -y
+
+echo 	\* Mise à jour du système
+apt-get dist-upgrade -y
+
+echo == Installation de git
+echo 	\* Installation du paquetage
+apt-get install -y git
+
+# Application de la configuration du proxy à GIT
+if [ "$http_proxy" != "" ]; then
+	echo 	\* Configuration GIT - Proxy HTTP : $http_proxy 
+	git config --global http.proxy $http_proxy
+fi
+
+if [ "$https_proxy" != "" ]; then
+	echo 	\* Configuration GIT - Proxy HTTPS : $https_proxy
+	git config --global https.proxy $https_proxy
+fi
+
+if [ "$ftp_proxy" != "" ]; then
+	echo 	\* Configuration GIT - Proxy FTP : $ftp_proxy
+	git config --global ftp.proxy $ftp_proxy
+fi
+
+# Clonage du dépot devstack
+echo == Récupération de Devstack
+echo 	\* Création répertoire : $REP_REPO_DEVSTACK
+rm -rf $REP_REPO_DEVSTACK
+mkdir $REP_REPO_DEVSTACK
+cd $REPO_REPO_DEVSTACK
+echo	\* Clonage dépot : $URL_DEVSTACK
+git clone -b $BRANCH $URL_DEVSTACK $REP_REPO_DEVSTACK 
+
+# configuration devstack
+echo == Configuration de l\'environement de Devstack - local.conf
+echo 	\* Rentre dans $REP_REPO_DEVSTACK
+cd $REP_REPO_DEVSTACK 
+echo 	\* Création fichier
+echo [[local\|localrc]] > $CONF_FILE
+for i in FIXED_RANGE NETWORK_GATEWAY LOGDAY LOGDIR LOGFILE ADMIN_PASSWORD DATABASE_PASSWORD RABBIT_PASSWORD SERVICE_PASSWORD SERVICE_TOKEN STACK_USER; do
+	echo $i=$(eval "echo \$$(echo $i)") >> $CONF_FILE
+	echo 	\* Configuration de $i=$(eval "echo \$$(echo $i)") 
+done
+
+# Path pour eviter l'usge de git
+echo == Patch de stackrc pour bannir l\'usage du protocol git
+sed s/git:\\/\\/git.openstack.org/https:\\/\\/git.openstack.org/g stackrc > stackrc.new
+mv stackrc stackrc.old
+cp stackrc.new stackrc
+
+echo == Lancement de devstack
+#Rectification des droits pour laisser Devstack accessible à l'utilisateur Vagrant
+echo 	\* Rectification des droits du depot cloné de devstack ...
+chown -R vagrant:vagrant  $REP_REPO_DEVSTACK
+
+#Lancement de devstack
+echo 	\* Lancement de devstack en tant que Vagrant
+# sudo -n -u $DEVSTACK_USER bash -c export
+sudo -n -u $DEVSTACK_USER bash -c "eval \"export USER=$DEVSTACK_USER && export HOME=~$DEVSTACK_USER && export http_proxy=$http_proxy && export https_proxy=$https_proxy && export ftp_proxy=$ftp_proxy && export no_proxy=\$(hostname -I | sed \"s/ /,/g\")$no_proxy && cd $REP_REPO_DEVSTACK && ./stack.sh \""
