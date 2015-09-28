08f614096612e9ffda6d561da850fcf6f7083076
maxime <compastiem@yahoo.fr>
2015-09-28 18:23:19 +0200

    Add some inter-controller implementation (nedded to stabilize the intra-ext code)

--- moonwebview/static/moon/app/inter-extension/inter-extension.controller.list.js
+++ moonwebview/static/moon/app/inter-extension/inter-extension.controller.list.js
@@ -11,9 +11,54 @@
 			.controller('InterExtensionListController', InterExtensionListController);
 	
 	function InterExtensionListController() {
-	
-		
-	
+
+    var list = this;
+
+
+
+		//for (var unbind in rootListeners) {
+		//	  $scope.$on('$destroy', rootListeners[unbind]);
+		//}
+
+		/*
+		 *
+		 */
+
+		function getExtraExtensions() {
+			return (list.extraExtensions) ? list.extraExtensions : [];
+		};
+
+		function hasExtraExtensions() {
+			return list.getExtraExtensions().length > 0;
+		};
+
+		function addExtraExtension(extraExtension) {
+			list.extraExtensions.push(extraExtension);
+		};
+
+		function deleteExtraExtension(extraExtension) {
+			list.extraExtensions = _.chain(list.extraExtensions).reject({_id: extraExtension._id}).value();
+		};
+
+		function refreshExtraExtensions() {
+
+			list.table.total(list.extraExtensions.length);
+			list.table.reload();
+
+		};
+
+		function updateExtraExtensions(extraExtension) {
+
+			_(list.extraExtensions).each(function(anExtraExtension) {
+        		if(anExtraExtension._id === extraExtension._id) {
+        			anExtraExtension = _.clone(extraExtension);
+        		}
+        	});
+
+			return list.extraExtensions;
+
+		};
+
 	};
 	
 })();
